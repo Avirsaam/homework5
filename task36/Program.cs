a1a0a3b3284575b5a9deda58212d7ab6913bf684
AvirSaam <den.demchenko@gmail.com>
2023-02-03 05:08:53 +0300

    task 34, task36 completed

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,28 +1,33 @@
 ﻿/*
 
-Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-[345, 897, 568, 234] -> 2
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
 
 */
 
 
 
-const int minRandomNumber = 100;
-const int maxRundomNumber = 999;
-const int arraySize = 8;
+const int minRandomNumber = 1;
+const int maxRundomNumber = 99;
+const int arraySize = 4;
 
-int eventNumberCount = 0;
+int sumOfOddIndexElements = 0;
 int []myArray = new int[arraySize];
+
 
 FillArrayWithRandomNumbers(minRandomNumber, maxRundomNumber, myArray);
 
-
 for (int i = 0; i < myArray.Length; i++)
 {
-    if (myArray[i] % 2 == 0) eventNumberCount++;
+    if (i % 2 != 0) sumOfOddIndexElements+=myArray[i];
 }
-Console.WriteLine($"{ArrayToPrettyString(myArray)} -> {eventNumberCount}");
+
+Console.WriteLine($"{ArrayToPrettyString(myArray)} -> {sumOfOddIndexElements}");
+
+
 
 
 //возвращает строку, содержащую элементы массива в 
@@ -52,4 +57,3 @@ void FillArrayWithRandomNumbers(int randomMin, int randomMax, int[] arrayToFill)
         arrayToFill[i] = rnd.Next(randomMin, randomMax);
     }
 }
-
